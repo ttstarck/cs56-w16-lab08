fd00066f673443433b19653d90ceb1a393bc2568
Tristan <ttstarck@umail.ucsb.edu>
2016-02-24 14:55:14 -0800

    Go Gauchos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,8 @@ public class Main {
 
     get("/hello", (req, res) -> "Hello World");
 
+    get("/ucsb", (req, res) -> "Go Gauchos");
+
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
